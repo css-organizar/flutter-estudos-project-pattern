dd77d110ca573121ce5eab057692ee3994760a22
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2021-09-25 02:57:34 -0300

    Melhoria nos controllers com os eventos "onInit()" e "onClose()"

--- app-base-GetX/lib/presentation/views/configuration/configuration_controller.dart
+++ app-base-GetX/lib/presentation/views/configuration/configuration_controller.dart
@@ -1,3 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class ConfigurationController extends GetxController {}
+class ConfigurationController extends GetxController {
+  @override
+  void onInit() {
+    super.onInit();
+    debugPrint('ConfigurationController - onInit');
+  }
+
+  @override
+  void onClose() {
+    debugPrint('ConfigurationController - onClose');
+    super.onClose();
+  }
+}
--- app-base-GetX/lib/presentation/views/home/home_controller.dart
+++ app-base-GetX/lib/presentation/views/home/home_controller.dart
@@ -1,3 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class HomeController extends GetxController {}
+class HomeController extends GetxController {
+  @override
+  void onInit() {
+    super.onInit();
+    debugPrint('HomeController - onInit');
+  }
+
+  @override
+  void onClose() {
+    debugPrint('HomeController - onClose');
+    super.onClose();
+  }
+}
